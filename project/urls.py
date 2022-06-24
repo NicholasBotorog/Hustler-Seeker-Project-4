dc8952202deea8c46d62ae5efced8a3847a21966
Nicholas <razvan.botorog@yahoo.com>
2022-06-24 19:21:42 +0300

    ready to deploy

--- project/urls.py
+++ project/urls.py
@@ -3,19 +3,11 @@ from django.urls import path, include, re_path # <-- added this new import re_pa
 from .views import index # <-- also new
 
 urlpatterns = [
-    #...your other views,
-    re_path(r'^.*$', index) # <-- have this come last using re path.
-]
-
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('jwt_auth.urls')),
     path('api/jobs/', include('jobs.urls')),
     path('api/tags/', include('tags.urls')),
     path('api/reviews/', include('reviews.urls')),
     path('api/aplication/', include('aplication.urls')),
+    re_path(r'^.*$', index)
 ]
